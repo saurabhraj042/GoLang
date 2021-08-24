e38772a74281f453226599e82c15ec6ba240dd84
saurabhraj042 <saurabhraj042@gmail.com>
2021-08-24 21:47:10 +0530

    add HelloAgain() and TestHelloAgain()

--- basics.go
+++ basics.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+const helloAgainPrefix = "Hello, "
+
+func HelloAgain(name string) string{
+	if name == ""{
+		name = "World"
+	}
+
+	return helloAgainPrefix + name
+}
+
 func Hello() string{
 	return "Hello World from outside the main() scope"
 }
--- basics_test.go
+++ basics_test.go
@@ -10,3 +10,27 @@ func TestHello(t *testing.T){
 		t.Errorf("got %q want %q",got,want)
 	}
 }
+
+func TestHelloAgain(t *testing.T){
+	assertCorrectMessage := func(t testing.TB, got, want string){
+		t.Helper()
+		
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := HelloAgain("Raj")
+		want := "Hello, Raj"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("empty string defaults to 'World'", func(t *testing.T) {
+		got := HelloAgain("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
