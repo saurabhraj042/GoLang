a93ddec3c645cfc91783742848f24ef2ee7056ad
saurabhraj042 <saurabhraj042@gmail.com>
2021-08-25 16:00:35 +0530

    refactor Sum() func

--- Arrays/sum.go
+++ Arrays/sum.go
@@ -2,5 +2,11 @@ package arrays
 
 // Function to add integers of an Array and return it's sum
 func Sum(numbers [5]int) int {
-	return 0
+	sum := 0
+
+	for i := 0; i < 5; i++ {
+		sum += numbers[i]
+	}
+
+	return sum
 }
