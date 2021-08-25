31efa562569289cda1f5768ad63f9f5656a500ac
saurabhraj042 <saurabhraj042@gmail.com>
2021-08-25 20:31:07 +0530

    Use Rectangle struct inside tests of rectangle methods

--- Structs-methods-interfaces/shapes_test.go
+++ Structs-methods-interfaces/shapes_test.go
@@ -4,7 +4,8 @@ import "testing"
 
 // Test for Perimeter() function
 func TestPerimeter(t *testing.T){
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle(10.0, 10.0)
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
@@ -14,7 +15,8 @@ func TestPerimeter(t *testing.T){
 
 // Test for Area() function
 func TestArea(t *testing.T){
-	got := Area(10.0, 10.0)
+	rectangle := Rectangle(10.0, 10.0)
+	got := Area(rectangle)
 	want := 100.0
 
 	if got != want {
