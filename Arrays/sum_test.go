c1aad23348048faa09409c1037289e41151c67e2
saurabhraj042 <saurabhraj042@gmail.com>
2021-08-25 16:41:09 +0530

    refactor TestSum() func

--- Arrays/sum_test.go
+++ Arrays/sum_test.go
@@ -4,12 +4,23 @@ import "testing"
 
 // Test for Sum() function of arrays
 func TestSum(t *testing.T){
-	numbers := [5]int{1,2,3,4,5}
-
-	got := Sum(numbers)
-	want := 15
-
-	if got != want {
-		t.Errorf("got %d want %d given %v", got, want, numbers)
+	assertCorrectMessage := func(t testing.TB, got, want int){
+		t.Helper()
+		
+		if got != want {
+			t.Errorf("got %d want %d ", got, want)
+		}
 	}
+
+	// Subtest for array of fixed size
+	t.Run("collection of fixed size", func(t *testing.T) {
+		numbers := [5]int{1, 2, 3, 4, 5}
+
+		got := Sum(numbers)
+		want := 15
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	
 }
