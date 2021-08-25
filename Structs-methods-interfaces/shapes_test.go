1a75caaa9fe4ccd71ce70f36d5459ab60e3ed1f0
saurabhraj042 <saurabhraj042@gmail.com>
2021-08-25 20:17:48 +0530

    add Test for Area() func

--- Structs-methods-interfaces/shapes_test.go
+++ Structs-methods-interfaces/shapes_test.go
@@ -11,3 +11,13 @@ func TestPerimeter(t *testing.T){
 		t.Errorf("got %.2f want %.2f", got, want)
 	}
 }
+
+// Test for Area() function
+func TestArea(t *testing.T){
+	got := Area(10.0, 10.0)
+	want := 200.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
