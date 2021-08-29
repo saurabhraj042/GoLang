7cee752233c8bc5df20411a1a9e18caf3758a94c
saurabhraj042 <saurabhraj042@gmail.com>
2021-08-29 18:44:28 +0530

    refactor Test Racer() && Racer() function

--- sample1/racer.go
+++ sample1/racer.go
@@ -3,19 +3,34 @@ package sample1
 import (
 	"net/http"
 	"time"
+	"fmt"
 )
 
-func Racer(slowUrl, fastUrl string) string {
-	slowDuration := measureResponseTime(slowUrl)
-	fastDuration := measureResponseTime(fastUrl)
+var tenSecondTimeout = 10 * time.Second
 
-	if slowDuration < fastDuration {
-		return slowUrl
-	}
-
-	return fastUrl
+func Racer(a, b string) (winner string, error error) {
+    return ConfigurableRacer(a, b, tenSecondTimeout)
 }
 
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
+    select {
+    case <-ping(a):
+        return a, nil
+    case <-ping(b):
+        return b, nil
+    case <-time.After(timeout):
+        return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+    }
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func ()  {
+		http.Get(url)
+		close(ch)
+	} ()
+	return ch
+}
 
 func measureResponseTime(url string) time.Duration {
 	start := time.Now()
