0928d98bb8cfe188e79c436517d504451e48702b
saurabhraj042 <saurabhraj042@gmail.com>
2021-08-29 17:52:44 +0530

    add Racer() function logic

--- sample1/racer.go
+++ sample1/racer.go
@@ -1,5 +1,24 @@
 package sample1
 
+import (
+	"net/http"
+	"time"
+)
+
 func Racer(slowUrl, fastUrl string) string {
+	slowDuration := measureResponseTime(slowUrl)
+	fastDuration := measureResponseTime(fastUrl)
+
+	if slowDuration < fastDuration {
+		return slowUrl
+	}
+
 	return fastUrl
+}
+
+
+func measureResponseTime(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
 }
